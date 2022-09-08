1ecdaea78ef34c876465da4a4b79347a3e17f83e
bigmaac <britneymacahilig@gmail.com>
2022-09-08 20:09:55 +0800

    Client page Prototype
    -About us
    -home
    -project

---
+++ client/Projects/project.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Login </title>
+	<link rel="stylesheet" href="../include/style.css">
+	<link rel="stylesheet" href="aboutus.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+	<!-- <script src="login.js"></script> -->
+</head>
+
+
+<body>
+	<div class="container-fluid">
+		<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+			<a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+				<img src="../../images/aevg.png" class="" height="45">
+			</a>
+
+			<ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+				<li><a href="#" class="nav-link px-2 link-dark">Home</a></li>
+				<li><a href="#" class="nav-link px-2 link-dark">About Us</a></li>
+				<li><a href="#" class="nav-link px-2 link-dark">Services</a></li>
+				<li><a href="#" class="nav-link px-2 link-secondary">Projects</a></li>
+
+			</ul>
+
+			<div class="col-md-3 text-end">
+				<button type="button" class="btn btn-primary">Sign-up</button>
+				<button type="button" class="btn btn-outline-primary me-2">Login</button>
+
+
+
+			</div>
+		</header>
+	</div>
+	<div class="nav-scroller py-1 mb-2">
+		<nav class="nav d-flex justify-content-center">
+			<a class="p-2 link-secondary" href="#">All</a>
+			<a class="p-2 link-secondary" href="#">Building</a>
+			<a class="p-2 link-secondary" href="#">Commercial</a>
+			<a class="p-2 link-secondary" href="#">Design</a>
+			<a class="p-2 link-secondary" href="#">Renovation</a>
+			<a class="p-2 link-secondary" href="#">Blueprints</a>
+			<a class="p-2 link-secondary" href="#">Residentials</a>
+
+		</nav>
+		<div class="album py-5 bg-light">
+			<div class="container">
+
+				<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-10">
+					<div class="col"><a href=""></a>
+						<div class="card text-bg-light">
+							<img src="../../images/q.png" class="card-img" alt="">
+							<div class="card-img-overlay">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+								<p class="card-text"><small>Last updated 3 mins ago</small></p>
+							</div>							
+						</div>
+					</div>
+				
+
+				
+					<div class="col">
+						<div class="card text-bg-dark">
+							<img src="../../images/q.png" class="card-img" alt="">
+							<div class="card-img-overlay">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+								<p class="card-text"><small>Last updated 3 mins ago</small></p>
+							</div>							
+						</div>
+					</div>
+				
+				
+					<div class="col">
+						<div class="card text-bg-dark">
+							<img src="../../images/q.png" class="card-img" alt="">
+							<div class="card-img-overlay">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+								<p class="card-text"><small>Last updated 3 mins ago</small></p>
+							</div>							
+						</div>
+					</div>
+			</div>
+				
+
+			<div class="container">
+				<footer class="py-3 my-4">
+					<ul class="nav justify-content-center border-bottom pb-3 mb-3">
+
+					</ul>
+					<p class="text-center text-muted">&copy; 2020 AEVG BUILDERS</p>
+
+				</footer>
+
+</body>
+
+</html> 
--- client/home/home.php
+++ client/home/home.php
@@ -2,9 +2,12 @@
 <html>
 
 <head>
-    <title>Home</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
     <link rel="stylesheet" href="../include/style.css">
-    <link rel="stylesheet" href="../home/home.css">
+    <link rel="stylesheet" href="home.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
@@ -12,22 +15,85 @@
 </head>
 
 <body>
-    <nav class="navbar container">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <!-- <img src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top"> -->
-                Bootstrap
-            </a>
-        </div>
 
-        <div>
-            <a href="home/home.php">HOME</a>
-        </div>
-    </nav>
-    <div class="container text-center p-5">
-        <div class="backimage container text-center p-5"></div>
-    </div>
+<div class="container-fluid">
+		<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+			<a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+				<img src="../../images/aevg.png" class="" height="45">
+			</a>
+
+			<ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+				<li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+				<li><a href="#" class="nav-link px-2 link-dark">About Us</a></li>
+				<li><a href="#" class="nav-link px-2 link-dark">Services</a></li>
+				<li><a href="#" class="nav-link px-2 link-dark">Projects</a></li>
+
+			</ul>
+
+			<div class="col-md-3 text-end">
+				<button type="button" class="btn btn-primary">Sign-up</button>
+				<button type="button" class="btn btn-outline-primary me-2">Login</button>
+
+			</div>
+		</header>
+	</div>
 
 </body>
+
+    <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <rect width="100%" height="100%" fill="#777" />
+                </svg>
+
+                <div class="container">
+                    <div class="carousel-caption text-start">
+                        <h1>Example headline.</h1>
+                        <p>Some representative placeholder content for the first slide of the carousel.</p>
+                        <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <rect width="100%" height="100%" fill="#777" />
+                </svg>
+
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1>Another example headline.</h1>
+                        <p>Some representative placeholder content for the second slide of the carousel.</p>
+                        <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <rect width="100%" height="100%" fill="#777" />
+                </svg>
+
+                <div class="container">
+                    <div class="carousel-caption text-end">
+                        <h1>One more for good measure.</h1>
+                        <p>Some representative placeholder content for the third slide of this carousel.</p>
+                        <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
 
 </html>
