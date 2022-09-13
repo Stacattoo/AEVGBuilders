462cf4a66dbe36c69d65ae2dd6d4b59e859e6f7d
Stacattoo <109016831+Stacattoo@users.noreply.github.com>
2022-09-13 20:59:34 +0800

    idk

--- client/home/home.php
+++ client/home/home.php
@@ -1,3 +1,8 @@
+<?php
+include_once("../include/dbh.inc.php");
+$dbh = new dbHandler;
+
+?>
 <!DOCTYPE html>
 <html>
 
@@ -21,6 +26,8 @@
             <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
                 <img src="../../images/aevg.png" class="" height="45">
             </a>
+
+            
 
             <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
                 <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
