1c6853f93ce5b8575ec67801e08fd9e89fdc8cab
Hanna Clarisse <sagunhannaclarisse@gmail.com>
2022-09-26 21:15:48 +0800

    edit admin 1.0.1

--- admin/HomeAdmin.php
+++ admin/HomeAdmin.php
@@ -11,8 +11,8 @@ $dbh = new dbHandler();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
 
+    <link rel="stylesheet" href="include/style.css">
     <link rel="stylesheet" href="admin.css">
-    <link rel="stylesheet" href="style.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
