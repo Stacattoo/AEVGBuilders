2a0bb2d68d9dc11e9dbfd0c0a7ae399a9e078b3e
Hanna Clarisse <sagunhannaclarisse@gmail.com>
2022-10-03 21:28:47 +0800

    Update schedule.sql

--- client/sql/schedule.sql
+++ client/sql/schedule.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 03, 2022 at 02:28 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.0
+-- Generation Time: Oct 03, 2022 at 03:28 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 7.4.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,6 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `schedule` (
   `id` int(12) NOT NULL,
   `user_id` int(12) NOT NULL,
+  `employee_id` int(255) NOT NULL,
   `dateTime` timestamp NOT NULL DEFAULT current_timestamp(),
   `reason` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL DEFAULT 'pending'
@@ -39,9 +40,9 @@ CREATE TABLE `schedule` (
 -- Dumping data for table `schedule`
 --
 
-INSERT INTO `schedule` (`id`, `user_id`, `dateTime`, `reason`, `status`) VALUES
-(1, 0, '2022-09-30 08:38:35', 'dasda', 'pending'),
-(2, 1, '2022-09-30 08:47:21', 'sa wakas huehue', 'pending');
+INSERT INTO `schedule` (`id`, `user_id`, `employee_id`, `dateTime`, `reason`, `status`) VALUES
+(1, 0, 0, '2022-09-30 08:38:35', 'dasda', 'pending'),
+(2, 1, 0, '2022-09-30 08:47:21', 'sa wakas huehue', 'pending');
 
 --
 -- Indexes for dumped tables
