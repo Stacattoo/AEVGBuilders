923995ebf813ae91983ed544b658f36048b089bb
Hanna Clarisse <sagunhannaclarisse@gmail.com>
2022-10-26 09:38:18 +0800

    stash

--- admin/materials/materials.js
+++ admin/materials/materials.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     $("#alertError").hide();
     $("#alertSuccess").hide();
 
+    refreshTable();
     $("#uploadMaterials").submit(function (event) {
         console.log("kahit ano");
         event.preventDefault();
@@ -23,7 +24,7 @@ $(document).ready(function () {
                     $("#alertSuccess").html(response.msg);
                     $("#alertSuccess").show();
                     $("#uploadMaterials").trigger("reset");
-
+                    refreshTable();
                 }
             }
         });
@@ -35,4 +36,107 @@ $(document).ready(function () {
         $("#alertSuccess").hide();
 
     });
+
+    function refreshTable() {
+        $.ajax({
+
+            type: "POST",
+            url: "materials/dispMaterials.php",
+            data: {
+                displayMaterials: true
+            },
+            dataType: "JSON",
+            success: function (response) {
+                console.log("harold");
+                var content = ``;
+                $.each(response, function (i, val) {
+                    console.log(val.id);
+                    content += `
+            <tr id="matChoose" class="materialContainer" value="${val.id}" name="select">
+            <th scope="row">${val.id}</th>
+            <td>${val.code}</td>
+            <td>${val.name}</td>
+            <td>${val.category}</td>
+            <td>${val.description}</td>
+        </tr>
+        `;
+                });
+                console.log(content);
+                $('#materialsList').html(content);
+
+                $('#table tr').click(function () {
+
+                    $(this).find('tr id:matChoose').prop('checked', true);
+                    $('#table tr').removeClass("table-primary");
+                    $(this).addClass("table-primary");
+                });
+
+                $('.materialContainer').click(function (e) { //pwede yata i call si #table tr
+                    e.preventDefault();
+                    materialsId = $(this).data("id");
+                    $('#hiddenId').val(materialsId);
+                    dataFilter = response.filter(function (eachEditInfo) {
+                        return eachEditInfo.id == materialsId;
+                    })[0];
+
+                    function imageRefresh() {
+                        let contentEdit = ``;
+                        $.each(dataFilter.image, function (indexInArray, data) {
+                            contentEdit += `
+                             <div class="col">
+                                    <div class="border position-relative">
+                                        <img src="../materials/${data}" class="d-block img-fluid img">
+                                        <span class="deleteImgBtn position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+                                            id="imageDeleteBtn"  data-id="${indexInArray}">
+                                            X
+                                        </span>
+                                       
+                                    </div>
+                                </div>
+                                <style> 
+                                    .deleteImgBtn {
+                                        cursor: pointer;
+                                    }
+
+                                    .deleteImgBtn:hover {
+                                        background: #461217 !important;
+                                    }
+                                </style>
+                             `
+                        });
+                        $('#view-editMats').html(contentEdit);
+                        $('#edit-image').val(dataFilter.image);
+
+                    }
+                    imageRefresh();
+                    $('#deleteBtn').attr("data-id", dataFilter.id);
+                    $('#hiddenId').data("id", dataFilter.id);
+                    $('#edit-title').val(dataFilter.title);
+                    $('#edit-category').val(dataFilter.category);
+                    $('#edit-description').html(dataFilter.description);
+                    $('#editMaterialsModal').modal("show");
+
+                    $('.deleteImgBtn').click(function (e) {
+                        e.preventDefault();
+                        deleteId = $(this).attr('data-id');
+                        imageSplice = dataFilter.image.splice(deleteId, 1);
+                        console.log(imageSplice);
+                        imageRefresh();
+
+                    });
+
+                    $("#alertErrorEdit").hide();
+                    $("#alertSuccessEdit").hide();
+
+                    //create dito ng function pag magsusubmit ng edit materials
+                });
+
+            },
+            error: function (response) {
+                console.log("hello");
+                console.log(response);
+            }
+        });
+    }
+
 });
--- admin/materials/materials.php
+++ admin/materials/materials.php
@@ -10,29 +10,34 @@ $dbh = new dbHandler;
 <div class="container-fluid">
     <h3><i class="fal fa-analytics me-2"></i>Upload Materials</h3>
     <hr>
-    <div class="container-fluid">
-        <div class="row justify-content-md">
-            <div class="col-md-auto">
-                <form id="uploadMaterials">
-                    <div class="input-group mb-3 mt-5">
-                        <h5>Product Code: &nbsp;</h4><input type="text" class="" name="code" placeholder="Code" aria-label="code" aria-describedby="basic-addon1" required>
-                    </div>
-                    <div class="input-group mb-3">
-                        <h5>Product Name: &nbsp;</h4><input type="text" class="" name="name" placeholder="Name" aria-label="name" aria-describedby="basic-addon1" required>
-                    </div>
-                    <div class="input-group mb-3">
-                        <h5>Product Category: &nbsp;</h4><select class="" aria-label="Default select example" name="category" required>
-                                <option selected>Catergory</option>
-                                <option value="Concrete">Concrete</option>
-                                <option value="Cement">Cement</option>
-                                <option value="Nails">Nails</option>
-                                <option value="Ply wood">Ply wood</option>
-                            </select>
-                    </div>
-                    <div class="input-group mb-3">
-                        <h5>Product Image: &nbsp;</h4>
-                            <input type="file" class="" name="image" placeholder="image" aria-label="image" aria-describedby="basic-addon1" required>
-                            <!-- <div class="card" name="image" id="uploadReset">
+    <form id="uploadMaterials" class="mb-5">
+        <div class="row ">
+            <div class="col">
+                <div class="input-group mb-3">
+                    <h5>Product Code: &nbsp;</h5>
+                    <input type="text" class="form-control" name="code" placeholder="Code" aria-label="code" aria-describedby="basic-addon1" required>
+                </div>
+                <div class="input-group mb-3">
+                    <h5>Product Name: &nbsp;</h5>
+                    <input type="text" class="form-control" name="name" placeholder="Name" aria-label="name" aria-describedby="basic-addon1" required>
+                </div>
+                <div class="input-group mb-3">
+                    <h5>Product Category: &nbsp;</h5>
+                    <select class="form-select" aria-label="Default select example" name="category" required>
+                        <option selected>Catergory</option>
+                        <option value="Concrete">Concrete</option>
+                        <option value="Cement">Cement</option>
+                        <option value="Nails">Nails</option>
+                        <option value="Ply wood">Ply wood</option>
+                    </select>
+                </div>
+
+            </div>
+            <div class="col">
+                <div class="input-group mb-3">
+                    <h5>Product Image: &nbsp;</h5>
+                    <input type="file" class="form-control" name="image" placeholder="image" aria-label="image" aria-describedby="basic-addon1" required>
+                    <!-- <div class="card" name="image" id="uploadReset">
                                 <div class="drag-area">
                                     <span class="visible">
                                         Drag & drop image here or
@@ -46,29 +51,31 @@ $dbh = new dbHandler;
                                 <div class="container" id="imgCon"></div>
 
                             </div> -->
-                    </div>
-                    <div class="input-group mb-3">
-                        <h5>Product Description: &nbsp;</h4><textarea class="" name="description" placeholder="Description" aria-label="With textarea"></textarea>
-                    </div>
-                    <!-- <div class="input-group mb-3">
+                </div>
+                <div class="input-group mb-3">
+                    <h5>Product Description: &nbsp;</h5><textarea class="form-control" name="description" placeholder="Description" aria-label="With textarea"></textarea>
+                </div>
+                <!-- <div class="input-group mb-3">
                         <h5>Product Stock: &nbsp;</h4><input type="number" class="" name="stock" placeholder="Number of stock" aria-label="stock" aria-describedby="basic-addon1" required>
                     </div> -->
 
-                    <div class="alert alert-danger mt-3" role="alert" id="alertError">
-                    </div>
-                    <div class="alert alert-success mt-3" role="alert" id="alertSuccess">
-                    </div>
+                <div class="alert alert-danger mt-3" role="alert" id="alertError">
+                </div>
+                <div class="alert alert-success mt-3" role="alert" id="alertSuccess">
+                </div>
 
-                    <button type="submit" class="btn btn-dark">Upload Product</button>
-                </form>
+                <button type="submit" class="btn btn-dark">Upload Product</button>
             </div>
         </div>
-    </div>
+
+    </form>
+
 
     <table id="table" class="table table-sm table-hover">
         <thead>
             <tr id="first-tr">
-                <th>Select</th>
+
+                <th>ID</th>
                 <th>Code</th>
                 <th>NAME</th>
                 <th>CATEGORY</th>
@@ -79,49 +86,72 @@ $dbh = new dbHandler;
 
         </tbody>
     </table>
+
 </div>
 
-<script>
-    // console.log("hello");
-    $(document).ready(function() {
-        // console.log("hi");
-        $.ajax({
-            
-            type: "POST",
-            url: "materials/dispMaterials.php",
-            data: {
-                displayMaterials: true
-            },
-            dataType: "JSON",
-            success: function(response) {
-                console.log("harold");
-                var content = ``;
-                $.each(response, function(i, val) {
-                     console.log(val.id);
-                    content += `
-                    <tr>
-                            <td><input type="radio" id="matChoose" value="${val.id}" name="select"></td>
-                            <td>${val.code}</td>
-                            <td>${val.name}</td>
-                            <td>${val.category}</td>
-                            <td>${val.description}</td>
-                        </tr>
-                    `;
-                });
-                console.log(content);
-                $('#materialsList').html(content);
+<!-- Modal -->
+<div class="modal fade" id="editMaterialsModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="materialTitle">Material Details</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form id="editUploadMaterials">
+                <div class="modal-body">
+                    <input type="hidden" id="hiddenId" name="hiddenId">
+                    <div class="container text-center">
+                        <div class="row row-cols-2 row-cols-lg-3 g-2 g-lg-3" id="view-editMats">
+                        </div>
+                    </div>
 
-                $('#table tr').click(function() {
+                    <div class="input-group mb-3 mt-5 row">
+                        <h5 class="col-sm-2 ">Material Code &nbsp;</h5>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="edit-title" name="titleEdit" placeholder="Title of Project" aria-label="title" aria-describedby="basic-addon1">
+                        </div>
+                        <!-- code should not editable -->
+                    </div>
+                    <div class="input-group mb-3 row">
+                        <h5 class="col-sm-2 ">Materials Category: &nbsp;</h5>
+                        <div class="col-sm-10">
+                            <select class="form-select" aria-label="Default select example" id="edit-category" name="categoryEdit">
+                                <option selected disabled>Catergory</option>
+                                <option value="Interior">Interior</option>
+                                <option value="Renovate">Renovate</option>
+                                <option value="Bungalo">Bungalo</option>
+                                <option value="Modern">Modern</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
 
-                    $(this).find('td input:radio').prop('checked', true);
-                    $('#table tr').removeClass("table-primary");
-                    $(this).addClass("table-primary");
-                })
-            }, error: function(response){
-                console.log("hello");
-                console.log(response);
-            }
-        });
+                        <h5 class="col-sm-2 ">Add Image: &nbsp;</h4>
+                            <div class="col-sm-10">
+                                <input type="file" class="form-control" id="unset" name="imageEdit[]" placeholder="image" aria-label="image" aria-describedby="basic-addon1" multiple>
+                                <input type="hidden" id="edit-image" name="imageEditStore" value="">
+                            </div>
+                    </div>
+                    <div class="input-group mb-3 row">
+                        <h5 class="col-sm-2 ">Material Description: &nbsp;</h5>
+                        <div class="col-sm-10">
+                            <textarea class="form-control" id="edit-description" name="descriptionEdit" placeholder="Description" aria-label="With textarea"></textarea>
+                        </div>
+                    </div>
+                </div>
 
-    });
-</script>
+                <div class="modal-footer">
+                    <div class="alert alert-danger mt-3" role="alert" id="alertErrorEdit">
+                    </div>
+                    <div class="alert alert-success mt-3" role="alert" id="alertSuccessEdit">
+                    </div>
+                    <button type="button" class="btn  btn-outline-danger" id="deleteBtn">Delete</a>
+                        <button type="submit" class="btn  btn-outline-success ">Save changes</button>
+                        <button type="button" class="btn btn-outline-dark" data-bs-dismiss="modal">Close</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
